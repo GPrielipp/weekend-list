cdfbc9dcb5cc3bc8b53d1fd7266b4ae2c03fad75
GPrielipp <goaliegeo30@gmail.com>
2026-01-26 10:22:36 -0500

    ?

--- frontend/src/Pages/Admin.js
+++ frontend/src/Pages/Admin.js
@@ -1,7 +1,32 @@
+import React, { useState } from 'react';
+import { read, utils } from 'xlsx';
+
 export default function Admin() {
+	const [fileData, setFile] = useState(null);
+
+	const validateFile = (event) => {
+		const file = event.target.files[0];
+
+		const spreadsheet = read(file);
+		const json = utils.sheet_to_json(spreadsheet);
+
+		setFile(json);
+	};
+
+	const handleFileUpload = async () => {
+		console.log(fileData);
+	};
+
 	return (
 		<>
 			<h1>Admin</h1>
+
+			<input
+				type="file"
+				accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.ms-excel"
+				onChange={validateFile}
+			/>
+			<button onClick={handleFileUpload}>Upload</button>
 		</>
 	);
 }
